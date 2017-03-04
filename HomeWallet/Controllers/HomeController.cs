720d40e85e0ebce70dce7289814a1aac11950fc1
Szymon Bakunowicz <szymon.bakunowicz@gmail.com>
2017-03-04 20:57:00 +0100

    Dynamically load new receipts in homepage

--- HomeWallet/Controllers/HomeController.cs
+++ HomeWallet/Controllers/HomeController.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeWallet.Data;
+using HomeWallet.Models.ReceiptViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeWallet.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -30,6 +40,50 @@ namespace HomeWallet.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        public IActionResult GetReceipts(string lastDate)
+        {
+            var model = new List<HomeReceiptViewModel>();
+            DateTime last;
+
+            if (lastDate == "0")
+            {
+                last = DateTime.Today.AddDays(1);
+            }
+            else
+            {
+                last = DateTime.Parse(lastDate);
+            }
+
+            var dates = _context.Receipts
+                .Where(r => r.PurchaseDate < last)
+                .Select(r => r.PurchaseDate)
+                .OrderBy(r=>r.Date)
+                .Distinct().Take(3);
+
+            foreach (var date in dates)
+            {
+                var receipts = _context.Receipts.Where(r => r.PurchaseDate == date).ToList();
+                foreach (var receipt in receipts)
+                {
+                    var homeReceiptViewModel = new HomeReceiptViewModel {Date = date};
+                    double value =0;
+                    foreach (var receiptProduct in receipt.ReceiptProducts)
+                    {
+                        value += (receiptProduct.Price * receiptProduct.Amount);
+                    }
+                    var showReceiptViewModel = new ShowReceiptViewModel()
+                    {
+                        ID = receipt.ID,
+                        Shop = receipt.Shop.Name,
+                        Value = value
+                    };
+                    homeReceiptViewModel.Receipts.Add(showReceiptViewModel);
+                    model.Add(homeReceiptViewModel);
+                }
+            }
+            return PartialView(model);
         }
     }
 }
---
+++ HomeWallet/Models/ReceiptViewModels/HomeReceiptViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWallet.Models.ReceiptViewModels
+{
+    public class HomeReceiptViewModel
+    {
+        public DateTime Date { get; set; }
+        public ICollection<ShowReceiptViewModel> Receipts { get; set; } = new List<ShowReceiptViewModel>();
+    }
+
+    public class ShowReceiptViewModel
+    {
+        public int ID { get; set; }
+        public string Shop { get; set; }
+        public double Value { get; set; }
+
+    }
+}
