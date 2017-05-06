595fc1816c95ad2473db3e06e990384ee82f7406
Szymon Bakunowicz <szymon.bakunowicz@gmail.com>
2017-05-06 23:11:55 +0200

    Details tabs

---
+++ HomeWallet/wwwroot/js/tabjs.js
@@ -0,0 +1,12 @@
+﻿$('#statisticstab').click(function () {
+    $(this).parent().addClass('active');
+    $('#detailstab').parent().removeClass('active');
+    $('#detailsdiv').hide();
+    $('#statisticsdiv').show();
+});
+$('#detailstab').click(function () {
+    $(this).parent().addClass('active');
+    $('#statisticstab').parent().removeClass('active');
+    $('#detailsdiv').show();
+    $('#statisticsdiv').hide();
+});
